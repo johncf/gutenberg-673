57f0c7e19bbf0db57c2637fea94738d038f4e96d
johncf <johncf@protonmail.com>
2018-03-01 19:07:41 +0530

    long buggy Xpage fixes

---
+++ patches/001.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+sed -i 's/^<Xpage=<hw>Ad.ver.sa.ry.*>$/<Xpage=26>/' 673.txt
+sed -i 's/^<Xpage=<cs><col>..92olian attachment.*>$/<Xpage=27>/' 673.txt
+sed -i 's/^<Xpage=<hw>Ale.hoof.<.hw> <tt>.*>$/<Xpage=38>/' 673.txt
+sed -i 's/^<Xpage=<hw>An.a.stal.tic<.hw>.*>$/<Xpage=54>/' 673.txt
+sed -i 's/^<Xpage=<hw>Anx.i.e.ty<.hw>.*>$/<Xpage=67>/' 673.txt
+sed -i 's/^<Xpage=<cs><col>Red admiral<.col>.*>$/<Xpage=1203>/' 673.txt
+sed -i 's/^<Xpage=<cs><mcol><col>Cabbage rose<.col>.*>$/<Xpage=1252>/' 673.txt
+sed -i 's/^<Xpage=<cs><col>Saint Andrew.s.*>$/<Xpage=1269>/' 673.txt
+sed -i 's/^<Xpage=<cs><col>Sand badger<.col>.*>$/<Xpage=1274>/' 673.txt
+sed -i 's/^<Xpage=<cs><mcol><col>Air spring<.col>.*>$/<Xpage=1394>/' 673.txt
+sed -i 's/^<Xpage=<cs><col>Vegetable alkali<.col>.*>$/<Xpage=1598>/' 673.txt
